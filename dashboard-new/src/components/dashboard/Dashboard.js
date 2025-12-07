8bbced53d6ceeae13d2bf6b27f798b64e490e425
jlpanetta1681 <jlpanetta1681@gmail.com>
2025-12-07 14:54:11 -0500

    Add missing Dashboard.js component

---
+++ dashboard-new/src/components/dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Dashboard = () => (
+  <div className="dashboard">
+    <h1>Dashboard</h1>
+    <p>Analytics dashboard component</p>
+  </div>
+);
+
+export default Dashboard;
