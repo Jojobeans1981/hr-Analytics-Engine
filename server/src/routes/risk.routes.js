71dcedde83208c832d3125d1ce6b72ca0b37e773
jlpanetta1681 <jlpanetta1681@gmail.com>
2025-12-07 14:40:45 -0500

    Fix syntax error in risk.routes.js and add missing route files

--- server/src/routes/risk.routes.js
+++ server/src/routes/risk.routes.js
@@ -1,10 +1,7 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const router = express_1.default.Router();
+const express = require("express");
+const router = express.Router();
 
 // Risk assessment routes
 router.get('/', (req, res) => {
@@ -20,28 +17,4 @@ router.get('/', (req, res) => {
     });
 });
 
-exports.default = router;
-EOFcat > src/routes/risk.routes.js << 'EOF'
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const router = express_1.default.Router();
-
-// Risk assessment routes
-router.get('/', (req, res) => {
-    res.json({
-        status: 'success',
-        message: 'Risk assessment API is operational',
-        version: '1.0.0',
-        endpoints: [
-            { method: 'GET', path: '/api/risk', description: 'Get risk overview' },
-            { method: 'POST', path: '/api/risk/assess', description: 'Assess new risk' },
-            { method: 'GET', path: '/api/risk/metrics', description: 'Get risk metrics' }
-        ]
-    });
-});
-
-exports.default = router;
+module.exports = router;
